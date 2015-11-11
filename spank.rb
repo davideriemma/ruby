6a544a73422df3c3734610278e299c4b0ef933c7
Maurizio De Magnis <root@olisti.co>
2015-11-11 10:06:10 +0100

    moves functions on top

--- spank.rb
+++ spank.rb
@@ -5,17 +5,6 @@ def get_integer_input
   gets.chomp.to_i
 end
 
-birth1 = get_integer_input
-birth2 = get_integer_input
-while birth2 > 12
-  puts ' immetti un mese che esista!!'
-  birth2 = get_integer_input
-end
-birth3 = get_integer_input
-while birth3.to_i > 31
-  puts 'non esiste un mese così lungo'
-  birth3 = get_integer_input
-end
 def bisesitle birth1
   if birth1%4 == 0 or birth1%400 == 0
     return true
@@ -23,6 +12,7 @@ def bisesitle birth1
     return false
   end
 end
+
 def birthday birth3, birth2
   conta30 = [11,02,04,06,9]
   conta31 = [01,01,05,07,8,10,12]
@@ -43,6 +33,7 @@ def birthday birth3, birth2
     end
   end
 end
+
 def bisrec bisesitle
   if bisesitle == 'true' and birth2 == conta30[1]
     if birth3 >= 30
@@ -58,6 +49,19 @@ def bisrec bisesitle
     end
   end
 end
+
+birth1 = get_integer_input
+birth2 = get_integer_input
+while birth2 > 12
+  puts ' immetti un mese che esista!!'
+  birth2 = get_integer_input
+end
+birth3 = get_integer_input
+while birth3.to_i > 31
+  puts 'non esiste un mese così lungo'
+  birth3 = get_integer_input
+end
+
 bisesitle birth1
 bisrec bisesitle
 dtbirth = Time.mktime(birth1,birth2,birth3)
