973b88267c77d04a71a42bdd36aea31fa0160437
Maurizio De Magnis <root@olisti.co>
2015-11-11 10:11:37 +0100

    converts if expressions

--- spank.rb
+++ spank.rb
@@ -5,8 +5,8 @@ def get_integer_input
   gets.chomp.to_i
 end
 
-  if birth1%4 == 0 or birth1%400 == 0
 def bisesitle(birth1)
+  if (birth1 % 4) == 0 || (birth1 % 400) == 0
     return true
   else
     return false
@@ -16,16 +16,16 @@ end
 def birthday(birth3, birth2)
   conta30 = [11,02,04,06,9]
   conta31 = [01,01,05,07,8,10,12]
-  if birth3 == 30 or birth3 == 31 #anche qui le conversioni sono trial
     compar= conta30.include?(birth2)
     compar2= conta31.include?(birth2)
-    if compar == true and birth3 > 30
+  if birth3 == 30 || birth3 == 31 #anche qui le conversioni sono trial
+    if compar == true && birth3 > 30
       puts 'non esiste un mese del genere'
       birth2 = get_integer_input
       birth3 = get_integer_input
       birthday(birth3,birth2)
     end
-    if compar2 == true and birth3 > 31
+    if compar2 == true && birth3 > 31
       puts ' on esiste un mese del genere'
       birth2 = get_integer_input
       birth3 = get_integer_input
@@ -35,13 +35,13 @@ def birthday(birth3, birth2)
 end
 
 def bisrec(bisesitle)
-  if bisesitle == 'true' and birth2 == conta30[1]
+  if bisesitle == 'true' && birth2 == conta30[1]
     if birth3 >= 30
       puts ' non esiste un bisestile di 30'
       birth3 = get_integer_input
       bisrec(bisestile)
     end
-  elsif bisesitle == 'false' and birth2 ==conta30[1]
+  elsif bisesitle == 'false' && birth2 == conta30[1]
     if birth3 >= 29
       puts ' febbraio ha solo 28 giorni'
       birth3 = get_integer_input
