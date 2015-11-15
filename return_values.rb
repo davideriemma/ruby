17aa81bb186137d8b2a5a1ee077989f2df6bb726
Maurizio De Magnis <root@olisti.co>
2015-11-15 20:28:46 +0100

    fixes the condition
    We should use logical ANDs instead of logical ORs.
    More specifically, it’s more idiomatic to use `&&` and `||` instead of
    `and` and `or`.

--- return_values.rb
+++ return_values.rb
@@ -4,7 +4,7 @@ def metodo
  puts 'premere 2 per aggiungere'.center(80)
  puts 'premere 3 per i crediti'.center(80)
   scelta = gets.chomp
-  while scelta != '1' or scelta != '2' or  scelta !='3'
+  while scelta != '1' && scelta != '2' && scelta !='3'
   puts 'ma cosa fai??'.center(80)
   scelta = gets.chomp
   return scelta
